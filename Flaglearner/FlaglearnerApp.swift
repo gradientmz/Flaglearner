a7de353ed741241e250f79560226d0582f5da088
Xinyun Zhou <xinyunzhouu@gmail.com>
2021-01-08 20:20:57 -0500

    Initial Commit

---
+++ Flaglearner/FlaglearnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaglearnerApp.swift
+//  Flaglearner
+//
+//  Created by Mark Zhou on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FlaglearnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
